bdb8d3ec965b8f0fdc731541ba178cc9b8730688
IhSA21 <ihsanomnih006@gmail.com>
2025-04-24 23:22:43 +0800

    2-crud-area: Add area management functionality with CRUD operations
    - Updated AreaController to handle GET, POST, PATCH, and DELETE requests for areas.
    - Implemented AreaService methods for interacting with the database using Prisma.
    - Added CreateAreaDto and UpdateAreaDto for data validation.
    - Integrated PrismaModule into AreaModule for database access.

--- src/area/area.service.ts
+++ src/area/area.service.ts
@@ -1,4 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateAreaDto } from './dto/create-area.dto';
+import { UpdateAreaDto } from './dto/update-area.dto';
 
 @Injectable()
-export class AreaService {}
+export class AreaService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  getAllAreas = async () => {
+    return this.prisma.area.findMany();
+  };
+
+  getAreaById = async (id: string) => {
+    return this.prisma.area.findFirst({
+      where: { id },
+    });
+  };
+
+  createArea = async (data: CreateAreaDto) => {
+    return this.prisma.area.create({
+      data,
+    });
+  };
+
+  updateArea = async (id: string, data: UpdateAreaDto) => {
+    return this.prisma.area.update({
+      where: { id },
+      data,
+    });
+  };
+
+  deleteArea = async (id: string) => {
+    return this.prisma.area.delete({
+      where: { id },
+    });
+  };
+}
