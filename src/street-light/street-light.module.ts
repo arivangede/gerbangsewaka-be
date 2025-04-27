d0da40845adb6f9b2661705d6f76b76e7aaa1bbc
IhSA21 <ihsanomnih006@gmail.com>
2025-04-27 15:08:42 +0800

    6-crud-street-light: Implement Street Light module with CRUD operations and unit tests

--- src/street-light/street-light.module.ts
+++ src/street-light/street-light.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { StreetLightController } from './street-light.controller';
 import { StreetLightService } from './street-light.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [StreetLightController],
-  providers: [StreetLightService]
+  providers: [StreetLightService],
+  exports: [StreetLightService]
 })
 export class StreetLightModule {}
